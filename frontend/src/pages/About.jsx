f4717545a2c46ad4ec3eb2f433e79737b84718fd
Kieu Hoa LInh <l_kieu@vision-net.co.jp>
2025-01-03 13:32:26 +0700

    completed frontend UI

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,9 +1,42 @@
 import React from 'react';
+import Title from '../components/Title';
+import { assets } from '../assets/assets';
+import NewsletterBox from '../components/NewsletterBox';
 
 const About = () => {
   return (
     <div>
-      
+      <div className='text-2xl text-center pt-8 border-t'>
+        <Title text1='ABOUT' text2='US' />
+      </div>
+      <div className="flex flex-col my-10 md:flex-row gap-16">
+        <img className='w-full md:max-w-[450px]' src={assets.about_img} alt="" />
+        <div className="flex flex-col justify-center gap-6 md:w-2/4 text-gray-600">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, delectus? Obcaecati, odio maxime error libero eveniet saepe enim ut natus laudantium sapiente est doloribus quis ab totam in? Id, dolores!</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe quas quis voluptatum nesciunt aspernatur id provident voluptatem distinctio quidem nulla! Ratione quam autem ullam distinctio amet cum impedit culpa accusamus!</p>
+          <b className='text-gray-800'>Our Missing</b>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa similique, tempora maiores ab nostrum illum accusantium atque laudantium velit neque, reiciendis alias officia consequuntur nam expedita unde? Maiores, labore recusandae.</p>
+
+        </div>
+      </div>
+      <div className="text-4xl py-4">
+        <Title text1='WHY' text2='CHOOSE US' />
+      </div>
+      <div className="flex flex-col md:flex-row text-sm mb-20">
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Lorem ipsum dolor sit amet, consectetur adipisicing elit..</b>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Lorem ipsum dolor sit amet, consectetur adipisicing elit..</b>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Lorem ipsum dolor sit amet, consectetur adipisicing elit..</b>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+      </div>
+      <NewsletterBox />
     </div>
   )
 }
--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React from "react";
+import Title from "../components/Title";
+import { assets } from "../assets/assets";
+import NewsletterBox from "../components/NewsletterBox";
 
 const Contact = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <div className="text-2xl text-center pt-10 border-t">
+        <Title text1="CONTACT" text2="US" />
+      </div>
+      <div className="flex flex-col md:flex-row gap-10 justify-center my-10 mb-28">
+        <img className="w-full md:max-w-[480px]" src={assets.contact_img} alt="" />
+        <div className="flex flex-col justify-center gap-6 items-start">
+          <p className="font-semibold text-xl text-gray-600">Our Store</p>
+          <p className="text-gray-500">60 C1 <br /> Phường 13, Quận Tân Bình, HCM</p>
+          <p className="text-gray-500">Tel: (+84)968-146-460 <br />Email: mr.linh1090@gmail.com</p>
+          <p className="font-semibold text-xl text-gray-600">Careers at Forever.</p>
+          <p className="text-gray-500">Learn more about our teams and job openings.</p>
+          <button className="border-none border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500">Explore Jobs</button>
+        </div>
+      </div>
+      <NewsletterBox />
+    </>
+  );
+};
 
-export default Contact
+export default Contact;
