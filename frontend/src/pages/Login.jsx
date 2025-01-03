53bd5cb68aa1acfc2ccf9a99f7a6745405a005b5
Kieu Hoa LInh <l_kieu@vision-net.co.jp>
2025-01-03 12:25:37 +0700

    finished login, sign up ui

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Login = () => {
+  const [currentState, setCurrentState] = useState('Login');
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log('submitted');
+  };
+  
   return (
-    <div>
-      
-    </div>
+    <form onSubmit={onSubmitHandler} className='flex flex-col items-center w-[90%] m-auto mt-14 sm:max-w-96 gap-4 text-gray-800'>
+      <div className="inline-flex items-center gap-2 mb-2 mt-10">
+        <p className='prata-regular text-3xl'>{currentState}</p>
+        <hr className='border-none h-[1.5px] w-8 bg-gray-800' />
+      </div>
+      {currentState !== 'Login' && <input className='w-full px-3 py-2 border border-gray-800' type="text" placeholder='Name' />}
+      <input className='w-full px-3 py-2 border border-gray-800' type="email" placeholder='Email' />
+      <input className='w-full px-3 py-2 border border-gray-800' type="password" placeholder='Password' />
+      <div className="w-full flex flex-col sm:flex-row justify-between text-sm mt-[-8px]">
+        <p className='cursor-pointer'>Forgot your password?</p>
+        {
+          currentState === 'Login' ? (
+            <p onClick={() => setCurrentState('Sign Up')} className='cursor-pointer'>Create an account</p>
+          ) : (
+            <p onClick={() => setCurrentState('Login')} className='cursor-pointer'>Already have an account?</p>
+          )
+        }
+      </div>
+      <button type='submit' className='border-none px-3 py-2 bg-black text-white font-light mt-4'>{ currentState === 'Login' ? 'Login' : 'Sign Up'}</button>
+    </form>
   )
 }
 
